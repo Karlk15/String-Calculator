22181af39cfc3d08d7257798b8d3015b185c351e
Karl K <Karlk15@ru.is>
2016-10-10 18:42:38 +0000

    part Four method that handles negative numbers with exception

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,5 +1,8 @@
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 /**
@@ -8,34 +11,81 @@ import static jdk.nashorn.internal.runtime.regexp.joni.Config.log;
 
 public class StringCalculator {
     @Test
-    public final void TestEmptyStringThenReturnValueIs0() {
+    public final void TestEmptyStringThenReturnValueIs0() throws Exception
+    {
         int sum = StringCalculator.add("");
         log.println(sum);
     }
     @Test
-    public final void TestStringWithOneIntegerReturnsSum() {
+    public final void TestStringWithOneIntegerReturnsSum() throws Exception
+    {
         int sum = StringCalculator.add("1");
         log.println("Sum: " + sum);
     }
     @Test
-    public final void TestTenNumbersThenReturnSum() {
+    public final void TestTenNumbersThenReturnSum() throws Exception
+    {
         int sum = StringCalculator.add("1,2,3,4,5,6,7,8,9,10");
         log.println("Sum: "+sum);
     }
     @Test
-    public final void TestStringWithNewLineInString() {
+    public final void TestStringWithNewLineInString() throws Exception
+    {
         int sum = StringCalculator.add("1,2,3,4,5\n6,7,8,9,10");
         log.println("Sum: "+sum);
     }
+    @Test
+    public final void TestStringWithOneNegativeNumberInString()
+    {
+        try
+        {
+            int sum = StringCalculator.add("-1,2");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public final void TestStringWithMultibleNegativeNumberInString()
+    {
+        try
+        {
+            int sum = StringCalculator.add("2,-4,3,-5");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-    public static int add(String numbers)
+    public static int add(String numbers) throws Exception
     {
         int sum = 0;
         String[] numberInArray = numbers.split(",|\n");
+        List<String> listNegNumbers = new ArrayList();
             for (String n : numberInArray)
             {
-                if(n.isEmpty())return 0;
-                sum += Integer.parseInt(n);
+                String nNegative = n;
+                if(n.isEmpty())
+                {
+                    return 0;
+                }
+                if(Integer.parseInt(nNegative) < 0)
+                {
+                    listNegNumbers.add(nNegative);
+                }
+                else
+                {
+                    sum += Integer.parseInt(n);
+                }
+            }
+            if(!listNegNumbers.isEmpty())
+            {
+                String negativeNumbers = "";
+                for(String nNegative: listNegNumbers )
+                {
+                    negativeNumbers += nNegative+",";
+                }
+                throw new Exception("Negatives not allowed: "+ negativeNumbers);
             }
         return sum;
     }
